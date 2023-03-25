74259628c626b72ac5b172cf45263847c8b30dd4
ka-hull <khull001@odu.edu>
2023-03-25 13:04:32 -0400

    adding lab2 section3 & available lab2 sections1&2

--- scripts/header.js
+++ scripts/header.js
@@ -97,7 +97,7 @@ class Header extends HTMLElement {
                                 </a>
                                 <ul class="dropdown-menu">
                                     <li><a class="dropdown-item" href="#labSection">Lab 1</a></li>
-                                    <li><a class="dropdown-item" href="#">Lab 2 (WIP)</a></li>
+                                    <li><a class="dropdown-item" href="#lab2Section">Lab 2</a></li>
                                     <li><a class="dropdown-item" href="#">Lab 3 (WIP)</a></li>
                                     <li><a class="dropdown-item" href="#">Lab 4 (WIP)</a></li>
                                     <li><a class="dropdown-item" href="#glossarySection">Glossary Page</a></li>
